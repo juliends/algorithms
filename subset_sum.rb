eceef09c6195d241b6004bac6ed1184c7201f46b
Julien Da Silva <julien.da.silva@hotmail.com>
2017-08-10 19:45:43 +0200

    checkpoint

--- subset_sum.rb
+++ subset_sum.rb
@@ -1,21 +1,26 @@
 a = [1,2,3,4,5]  
-arr = []
-target_number = 5
+results = []
+target_number = 6
 numOfCombinations = 0
+n = 3
 
 for i in 0..(a.length) do
   a.combination(i).each do |b|
     c = b.reduce(:+)
-      if c == target_number
+      if c == target_number && b.length == n
         numOfCombinations += 1
+        results << b
+        p b
+        p c
       end
   end
 end
+p results
 p numOfCombinations
 
 # -------------------------------
-arr = (-5..5).to_a
-def find_zero_sum_groups(arr, n)
-  (1..arr.length).map { |length| arr.combination(length).to_a.select { |candidate| candidate.reduce(:+) == 0 }}.flatten(1)
-end
-p find_zero_sum_groups(arr, 0)
+# arr = (-5..5).to_a
+# def find_zero_sum_groups(arr, n)
+#   (1..arr.length).map { |length| arr.combination(length).to_a.select { |candidate| candidate.reduce(:+) == 0 }}.flatten(1)
+# end
+# p find_zero_sum_groups(arr, 0)
