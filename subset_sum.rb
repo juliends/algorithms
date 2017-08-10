8b9b75824768f868acbb4011489396e8bb7da02f
Julien Da Silva <julien.da.silva@hotmail.com>
2017-08-10 19:46:54 +0200

    checkpoint

--- subset_sum.rb
+++ subset_sum.rb
@@ -19,6 +19,7 @@ p results
 p numOfCombinations
 
 # -------------------------------
+
 # arr = (-5..5).to_a
 # def find_zero_sum_groups(arr, n)
 #   (1..arr.length).map { |length| arr.combination(length).to_a.select { |candidate| candidate.reduce(:+) == 0 }}.flatten(1)
