29c8435177e5726336ec3e191e49c914fe7f262a
Julien Da Silva <julien.da.silva@hotmail.com>
2017-08-11 16:55:06 +0200

    checkpoint

--- pairs_sum.rb
+++ pairs_sum.rb
@@ -1,15 +1,29 @@
 def sum_pairs(ints, s)
-  (1...ints.size).each do |x|
-    (0...x).each do |y|
-      return [ints[y], ints[x]] if ints[x] + ints[y] == s
-    end
+  seen = {}
+  for i in ints do
+    # p i
+    # p [s-i, i]
+    # p seen
+    return [s-i, i] if seen[s-i]
+    seen[i] = true
   end
   nil
 end
 
-array = [1, 2, 3, 4, 1, 0]
-s = 2
+array = [10, 4, 1, 3, 7, 0]
+s = 4
 p sum_pairs(array, s)
+
+# -----------------------------------------------
+
+# def sum_pairs(ints, s)
+#   (1...ints.size).each do |x|
+#     (0...x).each do |y|
+#       return [ints[y], ints[x]] if ints[x] + ints[y] == s
+#     end
+#   end
+#   nil
+# end
 
 # -----------------------------------------------
 
